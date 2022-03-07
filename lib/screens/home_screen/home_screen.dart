d7f39235f345b7a2b642bf45583f241f1146f04b
Moti Bartov <moti.bartov@anagog.com>
2022-03-07 14:18:31 +0200

    Solution 3

--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,9 +1,20 @@
-
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Workshop"),
+      ),
+      body: Center(
+        child: Text("Hello Flutter"),
+      ),
+    );
   }
 }
