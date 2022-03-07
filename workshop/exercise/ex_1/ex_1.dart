1f81432750c3034b13cbb35a951ae26071f5c72c
Moti Bartov <moti.bartov@anagog.com>
2022-03-07 09:13:35 +0200

    Exercise 1 completed

--- workshop/exercise/ex_1/ex_1.dart
+++ workshop/exercise/ex_1/ex_1.dart
@@ -2,13 +2,4 @@
 /**
  *         Create Application
  */
- //TODO 1. Under the lib root folder, create application.dart file
- //TODO 2. Import 'package:flutter/material.dart' package
- //TODO 3. Create MyApp class and extends StatelessWidget
- //TODO 4. Override the build() method, and return a MaterialApp Widget
- //TODO 5. In the MaterialApp constructor add title "Flutter Workshop" in the title: argument
- //TODO 6. In the MaterialApp constructor set a Container() to the home: argument
- //TODO 7. Under the lib folder, create a single main.dart file
- //TODO 8. Import 'package:flutter/material.dart' package
- //TODO 9. Add void main() function and call runApp() method to run the MyApp just created
  //TODO 10. Open a terminal, type: flutter run -d all to run the app (You should get black screen now)
