07eaec66b7f6572d7dcd2eeda9357d464e86036f
Moti Bartov <moti.bartov@anagog.com>
2022-03-07 09:37:59 +0200

    Solution 2, completed

--- lib/application.dart
+++ lib/application.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_workshop/screens/home_screen/home_screen.dart';
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Flutter Workshop",
-      home: Container(),
+      home: HomeScreen(),
     );
   }
 
