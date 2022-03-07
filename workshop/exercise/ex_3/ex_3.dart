e4ce682ccc972aa33c9af5cf93acc60a7e86b986
Moti Bartov <moti.bartov@anagog.com>
2022-03-07 11:42:05 +0200

    Exercise 3 ready

---
+++ workshop/exercise/ex_3/ex_3.dart
@@ -0,0 +1,29 @@
+/**
+ *              Home Page Body
+ */
+ //TODO 1. Open the home_page.dart file
+ //TODO 2. In the build method, replace the Container() widget with a Scaffold() widget
+ //TODO 3. Add an appBar: argument to the Scaffold widget -> abbBar: AppBar()
+/**
+ *
+ *  return Scaffold(
+          appBar: AppBar(),
+       );)
+ *
+ */
+
+ //TODO 4. Go to the terminal where the app runs, hit 'r' to hot reload, you should now see
+ //TODO    a white screen with a blue toolbar.
+ //TODO 5. Add a title to the tool bar, expand the AppBar constructor, add a title: argument
+ //TODO    with a Text() widget., do hot reload again ('r')
+ //TODO 6. You should now see the title on the toolbar.
+ //TODO 7. In the Scaffold right after the appBar field, add a body: argument
+ //TODO 8. Set the body: argument to Center() widget
+ //TODO 9. Set the Center() child: widget with a Text().
+/**
+ *
+    Center(
+    child: Text("Hello Flutter")
+    )
+*/
+//TODO 10. Do hot reload or restart the app.
