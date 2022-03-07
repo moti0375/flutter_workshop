89de5708ef6727344ebc9bbe30cc30fe4804e15c
Moti Bartov <moti.bartov@anagog.com>
2022-03-07 17:11:31 +0200

    Debugging

--- workshop/exercise/ex_4/ex_4.dart
+++ workshop/exercise/ex_4/ex_4.dart
@@ -2,24 +2,39 @@
  *              Add a raised button
  */
 
-//TODO 7. Wrap the button with a Column, click on the ElevatedButton, press Alt+Enter, select wrap with Column
-//TODO 8. Hot reload, the button jumped to the top of the screen. That's because we didn't set the Column attributes
-//TODO 9. In the Column constructor above the children[] argument, add the following:
+//TODO  1. Go over the Text widget and click Alt+Enter
+//TODO  2. From the drop down menu, select 'Wrap with new widget' option
+//TODO  3. Rename the new widget to ElevatedButton, add a ',' after the Text widget inside the ElevatedButton
+///    child: ElevatedButton(child: Text("Hello Flutter"),)
+//TODO  4. Notice that ElevatedButton requires an onPressed callback, for now set it to be null
+//TODO  5. Reformat the code by pressing Alt+Cmd+L, notice the difference with or without the last comma ','
+/**
+    body: Center(
+    child: ElevatedButton(
+       child: Text("Hello Flutter"),
+       onPressed: null
+      ), //ElevatedButton
+    ) //Center
+*/
+//TODO 5. Do hot reload, notice the new disabled button. That's because the onPressed property is null
+//TODO 6. Lets set the button stretched to the screen width
+//TODO 7. Wrap the button with a Column, click on the RaisedButton, select wrap with Column
+//TODO 8. Hot reload, the button jumped to the left of the screen.
+//TODO 9. In the Column constructor, above the children[] property, add the following:
 ///    mainAxisAlignment: MainAxisAlignment.center,
 ///    mainAxisSize: MainAxisSize.max,
 ///    crossAxisAlignment: CrossAxisAlignment.stretch,
-///    Do hot reload.
-//TODO 10. The button is now stretched all the screen width
-//TODO 11. Add Padding to the button, select the button, repeat 1+2 and wrap it with new widget
-//TODO 12. Select Add Padding. Do hot reload
-//TODO 13. Enable the button by adding a onClick callback:
+//TODO 10.    Do hot reload.
+//TODO 11. The button is now stretched all the screen width
+//TODO 12. Add Padding to the button, select the button, repeat 1+2 and wrap it with new widget
+//TODO 13. Select Add padding. Do hot reload
+//TODO 14. Enable the button by adding a onClick callback:
 ///    In the RaisedButton constructor, add onPressed: callback:
 ///    onPressed: (){
 ///
 ///    }
-//TODO 14. Do hot reload ('r'), the button should now be enabled
-//TODO 15. Add a log message to the button pressed:
+//TODO 15. Do hot reload ('r'), the button should now be enabled
+//TODO 16. Add a log message to the button pressed:
 ///    in the onPressed: function add:
 ///    print("Button Pressed");
-
-//TODO 16. Do hot reload, see button color, press button and see log messages on the terminal.
+//TODO 17. Do hot reload, see button color, press button and see log messages on the terminal.
