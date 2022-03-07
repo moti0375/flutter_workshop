6b37531b3562e19e5910588809c1201e1c5e295d
Moti Bartov <moti.bartov@anagog.com>
2022-03-07 17:06:24 +0200

    Debugging

--- workshop/exercise/ex_4/ex_4.dart
+++ workshop/exercise/ex_4/ex_4.dart
@@ -1,3 +1,25 @@
 /**
  *              Add a raised button
  */
+
+//TODO 7. Wrap the button with a Column, click on the ElevatedButton, press Alt+Enter, select wrap with Column
+//TODO 8. Hot reload, the button jumped to the top of the screen. That's because we didn't set the Column attributes
+//TODO 9. In the Column constructor above the children[] argument, add the following:
+///    mainAxisAlignment: MainAxisAlignment.center,
+///    mainAxisSize: MainAxisSize.max,
+///    crossAxisAlignment: CrossAxisAlignment.stretch,
+///    Do hot reload.
+//TODO 10. The button is now stretched all the screen width
+//TODO 11. Add Padding to the button, select the button, repeat 1+2 and wrap it with new widget
+//TODO 12. Select Add Padding. Do hot reload
+//TODO 13. Enable the button by adding a onClick callback:
+///    In the RaisedButton constructor, add onPressed: callback:
+///    onPressed: (){
+///
+///    }
+//TODO 14. Do hot reload ('r'), the button should now be enabled
+//TODO 15. Add a log message to the button pressed:
+///    in the onPressed: function add:
+///    print("Button Pressed");
+
+//TODO 16. Do hot reload, see button color, press button and see log messages on the terminal.
--- workshop/exercise/ex_5/ex_5.dart
+++ workshop/exercise/ex_5/ex_5.dart
@@ -1,31 +1,32 @@
 /**
- *              Images Page - StatefulWidget
+ *    Images Page - StatefulWidget
  */
 
-//TODO 1. Under pages package, create new dart file: images_page.dart
-//TODO 2. In the new file create ImagesPage class,
-//TODO 3. Make the class extends StatefulWidget
-//TODO 4. Override the createState method.
-//TODO 5. In the images_page.dart, create another class ImagesPageState, extends State<ImagesPage>
-//TODO 6. In new class, override the build method
+//TODO 1. Under screens directory/package, create new gallery_screen directory
+//TODO 2. In the new gallery_screen directory, create new gallery_screen.dart file
+//TODO 3. In the new file create ImagesPage class,
+//TODO 4. Make the class extends StatefulWidget
+//TODO 5. Override the createState method.
+//TODO 6. In the images_page.dart, create another class ImagesPageState, extends State<ImagesPage>
+//TODO 7. In new class, override the build method
 
 ///  TIP: when dart file is empty, type 'stful' + enter, IDE will generate a
-/// StatefulWidget and a State template. Fill the class name and do imports as necessary.
+///  StatefulWidget and a State template. Fill the class name and do imports as necessary.
 ///
 
-//TODO 7. In the State class, replace the Container() with a Scaffold() widget.
-//TODO 8. Add an AppBar with a title: "Images page" (checkout Ex_3 for reminder)
-//TODO 9. Add a body, with a Container() widget
-//TODO 10. Expand the Container constructor, add padding:
+//TODO 8. In the State class, replace the Container() with a Scaffold() widget.
+//TODO 9. Add an AppBar with a title: "Images page" (checkout Ex_3 for reminder)
+//TODO 10. Add a body, with a Container() widget
+//TODO 11. Expand the Container constructor, add padding:
 ///    body: Container(
 ///    padding: const EdgeInsets.all(8.0), ...
-//TODO 11. Next, set the Container child to Column()
-//TODO 12. Set the Column attributes to:
+//TODO 12. Next, set the Container child to Column()
+//TODO 13. Set the Column attributes to:
 ///      child: Column(
 ///        mainAxisSize: MainAxisSize.max,
 ///        mainAxisAlignment: MainAxisAlignment.center,
 ///        crossAxisAlignment: CrossAxisAlignment.center ...
-//TODO 13. Add children empty array to the Column
+//TODO 14. Add children empty array to the Column
 ///    The Scaffold now should look like this:
 ///    Container(
 ///       padding: const EdgeInsets.all(8.0),
@@ -45,8 +46,8 @@
  * Lets use the Flutter Navigator to open the new ImagesPage
  */
 
-//TODO  14. In the home_page.dart file, go to the Login RaisedButton
-//TODO  15. In the onPressed: (){}, call the Flutter Navigator to open the ImagesPage
+//TODO  15. In the home_page.dart file, go to the Login RaisedButton
+//TODO  16. In the onPressed: (){}, call the Flutter Navigator to open the ImagesPage
 ///    Navigator.of(context).push(context, MaterialPageRoute(
 ///       builder: (context){ new ImagesPage() }
 ///    ))
@@ -59,8 +60,8 @@
 ///       ),
 ///     );
 ///    }
-//TODO  16. Import the ImagesPage(), use Alt+Enter
-//TODO  17. Do hot reload
-//TODO  18. Press the login button, the images page should be opened with white screen.
+//TODO  17. Import the ImagesPage(), use Alt+Enter
+//TODO  18. Do hot reload
+//TODO  19. Press the login button, the images page should be opened with white screen.
 
 
