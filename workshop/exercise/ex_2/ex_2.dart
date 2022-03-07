44aaf3ca54da3ed364b694d65ededcc69a240719
Moti Bartov <moti.bartov@anagog.com>
2022-03-07 09:49:48 +0200

    Fix ex2 structure

--- workshop/exercise/ex_2/ex_2.dart
+++ workshop/exercise/ex_2/ex_2.dart
@@ -2,11 +2,11 @@
  *              Home Page
  */
  //TODO 1. Under the lib directory, create new package with the name 'screens' (right click, New --> Directory/Package)
- //TODO 2. Under the new screens directory, create home_page package
- //TODO 3. Inside the home_page package, create new home_page.dart file
- //TODO 4. Open home_page.dart, create HomePage class which extends StatelessWidget
+ //TODO 2. Under the new screens directory, create home_screen package
+ //TODO 3. Inside the home_page package, create new home_screen.dart file
+ //TODO 4. Open home_screen.dart, create HomeScreen class which extends StatelessWidget
  //TODO 5. Override the build method
  //TODO 6. Return a Container() Widget
- //TODO 7. Go back to the application file and replace the home: argument to HomePage()
- //TODO 8. Import HomePage as necessary
+ //TODO 7. Go back to the application file and replace the home: argument to HomeScreen()
+ //TODO 8. Import HomeScreen as necessary
  //TODO 9. Run app again (Still black screen)
