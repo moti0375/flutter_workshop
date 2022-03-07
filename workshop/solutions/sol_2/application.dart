d20de070618053979c88469d0bf23773a5c9a4e3
Moti Bartov <moti.bartov@anagog.com>
2022-03-07 09:47:36 +0200

    Solution 2, completed with solution files

---
+++ workshop/solutions/sol_2/application.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_workshop/screens/home_screen/home_screen.dart';
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter workshop",
+      home: HomeScreen(),
+    );
+  }
+}
