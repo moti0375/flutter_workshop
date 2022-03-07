ee91eb468dbbc34d73f22c6fbabfdf5d8f1efadd
Moti Bartov <moti.bartov@anagog.com>
2022-03-07 09:22:13 +0200

    Exercise 2

---
+++ workshop/exercise/ex_2/ex_2.dart
@@ -0,0 +1,12 @@
+/**
+ *              Home Page
+ */
+ //TODO 1. Under lib directory, create new package with the name 'pages' (right click, Package)
+ //TODO 2. Under the new pages package, create home_page package
+ //TODO 3. Inside the home_page package, create new home_page.dart file
+ //TODO 4. Open home_page.dart, create HomePage class which extends StatelessWidget
+ //TODO 5. Override the build method
+ //TODO 6. Return a Container() Widget
+ //TODO 7. Go back to the application file and replace the home: argument to HomePage()
+ //TODO 8. Import HomePage as necessary
+ //TODO 9. Run app again (Still black screen)
