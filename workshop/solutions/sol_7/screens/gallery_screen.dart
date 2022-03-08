0b091748e9ce609bddfc8478de4011b483d40011
Moti Bartov <moti.bartov@anagog.com>
2022-03-08 10:54:37 +0200

    Solution 7 update

---
+++ workshop/solutions/sol_7/screens/gallery_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class GalleryScreen extends StatefulWidget {
+  const GalleryScreen({Key? key}) : super(key: key);
+
+  @override
+  _GalleryScreenState createState() => _GalleryScreenState();
+}
+
+class _GalleryScreenState extends State<GalleryScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gallery Screen"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(8),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.network(
+              "https://image.tmdb.org/t/p/w500/xvx4Yhf0DVH8G4LzNISpMfFBDy2.jpg",
+              fit: BoxFit.fill,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 4),
+                    child: ElevatedButton(onPressed: (){}, child: Text("Previous")),
+                  ),
+                ),
+                Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      child: ElevatedButton(onPressed: (){}, child: Text("Next")),
+                    ))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
