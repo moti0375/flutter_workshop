338902c35dafcd7c78984e307ef4f5f50063a794
Moti Bartov <moti.bartov@anagog.com>
2022-03-13 16:05:51 +0200

    Exercise 12 fix

--- workshop/exercise/ex_12/ex_12.dart
+++ workshop/exercise/ex_12/ex_12.dart
@@ -1,34 +1,34 @@
 /**
  * Theme and Dark Mode
  *
- * Flutter has a build in Themeing framework which allows us to style our app it a modular way!
+ * Flutter has a built in Themeing framework which allows us to style our app it a modular way!
  * In addition, it also has support for Dark mode.
  *
  * In this chapter we will set our app theme both for light and dark mode
- * and set buttons default shape and corners
+ * and set buttons default shape and corners and text default size across the amm
  * */
 
-//TODO 1 - Open application.dart file, reminder, in this file we set our app root Widget, the MaterialApp
-///        This widget has plenty of properties which effect the entire app,
+//TODO 1 - Open application.dart file, reminder, in this file where we created our app root Widget, the MaterialApp
+///        This widget has plenty of properties which effect the entire app, such as orientation, navigation routes, themes and more.
 ///        We will use the MaterialApp properties to give themes to our app.
 //TODO 2 - In the MaterialApp constructor, add theme: property, the theme property receives ThemeData object which contains
-///        may theme properties such as colorScheme, textTheme and more.
+///        theme properties such as colorScheme, textTheme and more.
 //TODO 3 - Set the theme: property as follow ThemeData.from(colorScheme: ColorScheme.light(primary: Colors.white, onPrimary: Colors.black))
 //TODO 4 - Lets add some more styling by using the copyWith pattern in the end of the ThemeData.from()
 //TODO 5 - Set appBar elevation to 0 with AppBarTheme
-/// ThemeData.from().copyWith(appBarTheme: AppBarTheme(elevation: 0))
-//TODO 6 - Set TextTheme
-/// ThemeData.from().copyWith(appBarTheme: ...
-///                           textTheme: TextTheme(button: TextStyle(fontSize: 25), headline6: TextStyle(fontSize: 22)))
-//TODO 7 - Set ElevatedButtonTheme
+/// ThemeData.from(colorScheme: ...).copyWith(appBarTheme: AppBarTheme(elevation: 0))
+//TODO 6 - Set TextTheme, we want to set all buttons text to size of 25, and appBar text to 22 (appBar text is defined with headline6 theme attribute)
+/// ThemeData.from(colorScheme: ...).copyWith(appBarTheme: ...
+///                                          textTheme: TextTheme(button: TextStyle(fontSize: 25), headline6: TextStyle(fontSize: 22),),)
+//TODO 7 - Set ElevatedButtonTheme, set primary color and onPrimary (which set the button text color) and also the button shape
 ///
-/// ThemeData.from().copyWith(appBarTheme: ...
+/// ThemeData.from(colorScheme: ...).copyWith(appBarTheme: ...
 ///                           textTheme: TextTheme(button: TextStyle(fontSize: 25), headline6: TextStyle(fontSize: 22)),
 ///                           elevatedButtonTheme: ElevatedButtonThemeData(
-///                           style: ElevatedButton.styleFrom(
-///                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(40))),
-///                           primary: Colors.lightBlue,
-///                           onPrimary: Colors.white,
+///                                style: ElevatedButton.styleFrom(
+///                                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(40)),),
+///                                    primary: Colors.lightBlue,
+///                                    onPrimary: Colors.white,
 ///                         ),
 ///                        ),
 ///                       )
@@ -37,13 +37,13 @@
 /// theme: ThemeData.from(colorScheme: ColorScheme.light(primary: Colors.white, onPrimary: Colors.black)).copyWith(appBarTheme: ...
 ///                           textTheme: TextTheme(button: TextStyle(fontSize: 25), headline6: TextStyle(fontSize: 22)),
 ///                           elevatedButtonTheme: ElevatedButtonThemeData(
-///                           style: ElevatedButton.styleFrom(
-///                           shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(40))),
-///                           primary: Colors.lightBlue,
-///                           onPrimary: Colors.white,
-///                         ),
-///                        ),
-///                       )
+///                                        style: ElevatedButton.styleFrom(
+///                                                     shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(40))),
+///                                        primary: Colors.lightBlue,
+///                                        onPrimary: Colors.white,
+///                                     ),
+///                                   ),
+///                                 )
 //TODO 9 - Above the theme: property, add darkTheme: property
 ///   darkTheme:
 ///   theme: ThemeData.from...
@@ -59,12 +59,14 @@
 ///            )),
 ///         );
 ///
+///
 /**
  *  Extract the theme to methods
+ *  Lets create methods for providing the themes and use them in the MaterialApp
  * */
 
 //TODO 11 - Click on the light theme content ThemeData.from(colorScheme: ColorScheme.light...
-//TODO 12 - Press and hold Option while click the up arrow on the keyboard, this will start to mark the ThemeData content
+//TODO 12 - Press and hold Option while click the up arrow on the keyboard, this will start to mark the ThemeData content, mark the entire content of the ThemeData object
 //TODO 13 - Press Option+Cmd+M, this will open the extract method dialog
 //TODO 14 - Set method name to _buildLightTheme and press Enter
 //TODO 15 - Repeat steps 11 - 14 for darkTheme property, name the method _buildDarkTheme
@@ -77,10 +79,11 @@
 ///     );
 
 /**
- *  Remove the shape attribute from the CustomElevatedButton
+ *  Removing the style: attribute from the CustomElevatedButton
+ *
  *  Remember that we set the buttons rounded corners from the CustomElevatedButton, but this is no
- *  longer required as we are now gives the buttons stying from the app theme.
- *  That's why we should remove the styling from the class itself
+ *  longer required as we are now providing the buttons stying from the app theme.
+ *  That's why we should remove the styling from the class itself, as it will override the theme which comes from the App theme
  *
  * */
 
@@ -90,5 +93,5 @@
 ///         child: loading ? CircularProgressIndicator() : this.child,
 ///         onPressed: ...
 ///         )
-//TODO 19 - Do Hot reload, checkout the buttons, appBar and text stying
+//TODO 19 - Do Hot reload, checkout the buttons, appBar and text styling
 //TODO 20 - From the device settings, switch to dark mode and watch how app look changed with dark theme
