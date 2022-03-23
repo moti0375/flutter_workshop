24fc6c8292db58e7ee2bef16497a67d2f00cb95e
Moti Bartov <moti.bartov@anagog.com>
2022-03-23 10:45:56 +0200

    Ex 4 fix

--- workshop/exercise/ex_4/ex_4.dart
+++ workshop/exercise/ex_4/ex_4.dart
@@ -1,5 +1,5 @@
 /**
- *              Add a raised button
+ *              Add Elevated Button
  */
 
 //TODO  1. Go over the Text widget and click Alt+Enter
@@ -16,25 +16,25 @@
       ), //ElevatedButton
     ) //Center
 */
-//TODO 5. Do hot reload, notice the new disabled button. That's because the onPressed property is null
-//TODO 6. Lets set the button stretched to the screen width
-//TODO 7. Wrap the button with a Column, click on the RaisedButton, select wrap with Column
-//TODO 8. Hot reload, the button jumped to the left of the screen.
-//TODO 9. In the Column constructor, above the children[] property, add the following:
+//TODO 6. Do hot reload, notice the new disabled button. That's because the onPressed property is null
+//TODO 7. Lets set the button stretched to the screen width
+//TODO 8. Wrap the button with a Column, click on the RaisedButton, select wrap with Column
+//TODO 9. Hot reload, the button jumped to the left of the screen.
+//TODO 10. In the Column constructor, above the children[] property, add the following:
 ///    mainAxisAlignment: MainAxisAlignment.center,
 ///    mainAxisSize: MainAxisSize.max,
 ///    crossAxisAlignment: CrossAxisAlignment.stretch,
-//TODO 10.    Do hot reload.
-//TODO 11. The button is now stretched all the screen width
-//TODO 12. Add Padding to the button, select the button, repeat 1+2 and wrap it with new widget
-//TODO 13. Select Add padding. Do hot reload
-//TODO 14. Enable the button by adding a onClick callback:
+//TODO 11.    Do hot reload.
+//TODO 12. The button is now stretched all the screen width
+//TODO 13. Add Padding to the button, select the button, repeat 1+2 and wrap it with new widget
+//TODO 14. Select Add padding. Do hot reload
+//TODO 15. Enable the button by adding a onClick callback:
 ///    In the RaisedButton constructor, add onPressed: callback:
 ///    onPressed: (){
 ///
 ///    }
-//TODO 15. Do hot reload ('r'), the button should now be enabled
-//TODO 16. Add a log message to the button pressed:
+//TODO 16. Do hot reload ('r'), the button should now be enabled
+//TODO 17. Add a log message to the button pressed:
 ///    in the onPressed: function add:
 ///    print("Button Pressed");
-//TODO 17. Do hot reload, see button color, press button and see log messages on the terminal.
+//TODO 18. Do hot reload, see button color, press button and see log messages on the terminal.
