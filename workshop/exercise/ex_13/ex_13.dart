b77d2281f6576861d0041ee18ec8d3292a3f3b67
Moti Bartov <moti.bartov@anagog.com>
2022-04-10 13:19:31 +0300

    Ex 13 fixes

--- workshop/exercise/ex_13/ex_13.dart
+++ workshop/exercise/ex_13/ex_13.dart
@@ -19,10 +19,11 @@
 /**
  *  Adding dependencies
  * */
-//TODO 1 - Add the flutter_mobx library to the project dependencies, in the pubspec.yaml file: https://pub.dev/packages/flutter_mobx
-//TODO 2 - Add mobx_codegen dependency
-//TODO 3 - Add Provider package dependency
-//TODO 4 - Add build_runner to dev_dependencies,
+//TODO 1. Open the project pubspec.yaml file. In this file we can define dependencies, set our project version, define assets and more!
+//TODO 2. Add the flutter_mobx library to the project dependencies, in the pubspec.yaml file: https://pub.dev/packages/flutter_mobx
+//TODO 3. Add mobx_codegen dependency
+//TODO 4. Add Provider package dependency
+//TODO 5. Add build_runner to dev_dependencies,
 /// Checkout pubspec.yaml file with all new dependencies
 
 /// dependencies:
@@ -40,16 +41,16 @@
 ///     sdk: flutter
 ///   build_runner:
 
-//TODO 5 - After adding dependencies in the pubspec.yaml, click Pub get to sync the dependencies
-//TODO 6 - In the gallery_screen directory, create new gallery_store.dart file
-//TODO 7 - At the top of the file, import the mobx.dart package:
+//TODO 6. - After adding dependencies in the pubspec.yaml, click Pub get to sync the dependencies
+//TODO 7. - In the gallery_screen directory, create new gallery_store.dart file
+//TODO 8. - At the top of the file, import the mobx.dart package:
 /// import 'package:mobx/mobx.dart';
-//TODO 8 - In the new file, create an abstract class _GalleryStore, this will be the class which will contain the logic for the gallery
+//TODO 9. - In the new file, create an abstract class _GalleryStore, this will be the class which will contain the logic for the gallery
 /// abstract class _GalleryStore with Store {
 ///
 /// }
-//TODO 9 - Lets fill the store file code
-//TODO 10 - Copy the images list from the gallery page and place it inside the GalleryStore class
+//TODO 9. Lets fill the store file code
+//TODO 10. Copy the images list from the gallery screen and place it inside _GalleryStore class
 /// _GalleryStore with Store{
 ///
 ///   List<String> _images = [
@@ -58,20 +59,20 @@
 ///     "https://image.tmdb.org/t/p/w500/iiZZdoQBEYBv6id8su7ImL0oCbD.jpg"
 ///   ];
 ///
-//TODO 11 - Mobx store works with predefined components types such as: @computed, @action, @observable
-//TODO 12 - Below the _images list, Create the _index @observable:
+//TODO 11. Mobx store works with predefined components types such as: @computed, @action, @observable
+//TODO 12. Below the _images list, Create the _index @observable:
 /// @observable
 /// int _index = 0;
-//TODO 13 - Next, create the _image @computed field:
+//TODO 13. Next, create the _image @computed field:
 ///   @computed
 ///   String get image => _images[_index];
-//TODO 14 - Next, create first and last @computed fields:
+//TODO 14. Next, create first and last @computed fields:
 /// @computed
 /// bool get first => _index == 0;
 ///
 /// @computed
 /// bool get last => _index == (_images.length - 1);
-//TODO 15 - Create two @action methods for next and previous, these methods will be called from the Widget when user clicks the gallery buttons
+//TODO 15. Create two @action methods for next and previous, these methods will be called from the Widget when user clicks the gallery buttons
 /// @action
 ///   void next(){
 ///     _index++;
@@ -81,16 +82,16 @@
 ///   void previous(){
 ///     _index--;
 ///   }
-//TODO 16 - Our store class is ready, however, as you notice, this is an abstract private class, we should create a concrete class for it
-//TODO 17 - At the top of the file, above the abstract class define a concrete class for it:
+//TODO 16. Our store class is ready, however, as you notice, this is an abstract private class, we should create a concrete class for it
+//TODO 17. At the top of the file, above the abstract class define a concrete class for it:
 /// class GalleryStore = _GalleryStore with _$GalleryStore;
 ///
 /// abstract class _GalleryStore with Store{...
-//TODO 18 - You may get a compilation error, this is fine for now.
-//TODO 19 - Add the part statement below the file import:
+//TODO 18. You may get a compilation error, this is fine for now.
+//TODO 19. Add the part statement below the file import:
 /// import 'package:mobx/mobx.dart';
 /// part 'gallery_store.g.dart';
-//TODO 20 - Notice that currently you should have compilation error as the gallery_store.g.dart file not exists yet.
+//TODO 20. Notice that currently you should have compilation error as the gallery_store.g.dart file not exists yet.
 
 /**
  * Running the build runner script to generate code.
@@ -103,10 +104,10 @@
  *
  * */
 
-//TODO 21 - Open terminal in the project directory
-//TODO 22 - Run the following command to run the build runner script:
+//TODO 21. Open terminal in the project directory
+//TODO 22. Run the following command to run the build runner script:
 /// flutter pub run build_runner build
-//TODO 23 - Wait for the script to complete with Succeeded result:
+//TODO 23. Wait for the script to complete with Succeeded result:
 ///   [INFO] Succeeded after 11.9s with 2 outputs (15 actions)
-//TODO 24 - Go back to the gallery_store.dart file, see that all errors are gone and look for the gallery_store.g.dart file that was created in the same directory.
-//TODO 25 - As we've created the store, it still not in use, the GalleryScreen still works with setState, in the next lesson we will integrated the Store using the Provider library.
+//TODO 24. Go back to the gallery_store.dart file, see that all errors are gone and look for the gallery_store.g.dart file that was created in the same directory.
+//TODO 25. As we've created the store, it still not in use, the GalleryScreen still works with setState, in the next lesson we will integrated the Store using the Provider library.
