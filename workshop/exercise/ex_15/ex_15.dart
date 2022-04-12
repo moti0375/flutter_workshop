ca6e76d307d1261aac8435e6b7e52302951643bf
Moti Bartov <moti.bartov@anagog.com>
2022-04-12 10:39:36 +0300

    Clean ex_15 todos

--- workshop/exercise/ex_15/ex_15.dart
+++ workshop/exercise/ex_15/ex_15.dart
@@ -15,26 +15,5 @@
     Nevertheless, for this tutorial app, we can use the named routes api.
  * */
 
-//TODO 1. Under the lib folder, create new app_routes.dart file
-//TODO 2. In the new app_routes.dart file create 2 global routes constants
-/// const HOME_SCREEN = './';
-/// const GALLERY_SCREEN = '/gallery_screen';
-//TODO 3. Open the application.dart file
-//TODO 4. In the MaterialApp constructor, above the themes properties, add initialRoute: HOME_SCREEN
-//TODO 5. Next, add routes: property
-/// routes: {
-///   INITIAL_ROUTE: (context) => HomeScreen(),
-///   GALLERY_ROUTE: (context) => GalleryScreen()
-/// }
-//TODO 6. After we define the app routes, delete the home: property
-
-/**
- *   Use pushNamed api to navigate GalleryScreen
- */
-
-//TODO 7. In the home_screen.dart, replace the Navigator.of(context).push() with .pushNamed()
-/// Navigator.pushNamed(context, GALLERY_ROUTE)
-//TODO 8. Do hot reload
-//TODO 9. Check that in-app navigation works properly
 
 
